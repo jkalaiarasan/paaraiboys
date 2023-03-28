a985c7a820194a0d41b378caf7154b318b5dfb7c
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2023-03-29 00:04:54 +0530

    photos added

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,33 +1,29 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import members from '../data/members.json';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit';
 
 const Members = () => {
-    let members = [
-        { name: 'V.லோகேஷ்', age: 9, title: 'தலைவர்'},
-        { name: 'ஹரிஷ்', age : 11, title: 'உறுப்பினர்'},
-        { name: 'சந்தோஷ்', age: 10, title: 'உறுப்பினர்'},
-        { name: 'சக்திவேல் ', age: 8, title: 'உறுப்பினர்'},
-        { name: 'கோகுல்', age: 16, title: 'உறுப்பினர்'},
-        { name: 'சந்துரு', age : 5, title: 'உறுப்பினர்'},
-        { name: 'லோகித்', age: 6, title: 'உறுப்பினர்'},
-        { name: 'ரோஹித்', age: 6, title: 'உறுப்பினர்'},
-        { name: 'நவீன் பட்டயார்', age: 12, title: 'உறுப்பினர்'},
-        { name: 'MP தர்மர்', age: 36, title: 'உறுப்பினர்'},
-        { name: 'அஜித்', age: 25, title: 'உறுப்பினர்'},
-        { name: 'சிவா', age: 24, title: 'உறுப்பினர்'},
-        { name: 'சிவ குமார்', age: 27, title: 'உறுப்பினர்'},
-        { name: 'P கோனேரி', age: 40, title: 'உறுப்பினர்'},
-        { name: 'நவீன் தவான்', age: 16, title: 'உறுப்பினர்'},
-        { name: 'மோகன்', age: 10, title: 'உறுப்பினர்'},
-        { name: 'T.பிரசன்னா', age: 10, title: 'உறுப்பினர்'},
-        { name: 'K.பிரசன்னா', age: 10, title: 'உறுப்பினர்'},
-        { name: 'திருப்பதி', age: 15, title: 'உறுப்பினர்'},
-      ]
+    function importAll(r) {
+        return r.keys().map(r);
+    }
+
+    const images = importAll(require.context('../photos', false, /\.(png|jpe?g|svg)$/));
+    console.log('images ', images)
+    members.forEach(member => {
+        if(member.whatsapp){
+            member.whatsapp = 'https://wa.me/' +  member.whatsapp + '?text=Hi%20' + member.name + ',%20FROM%20பாறை%20பாய்ஸ்!';
+        }
+        if(member.photo){
+            member.photoPath = `/static/media/${member.photo}.${member.photoId}.${member.photoType}`;
+        } else {
+            member.photoPath = 'https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-20.jpg';
+        }
+    })
     return (
         <div>
             <div className="vh-100">
                 {members.map((member) => {
-                    return <MDBContainer>
+                    return <MDBContainer key={member.Id}>
                         <MDBRow className="justify-content-center">
                         <MDBCol md="9" lg="7" xl="5" className="mt-5">
                             <MDBCard style={{ borderRadius: '15px' }}>
@@ -35,8 +31,8 @@ const Members = () => {
                                 <div className="d-flex text-black">
                                 <div className="flex-shrink-0">
                                     <MDBCardImage
-                                    style={{ width: '180px', borderRadius: '10px' }}
-                                    src='https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-20.jpg'
+                                    style={{ width: '180px', borderRadius: '10px', height: '200px' }}
+                                    src={member.photoPath}
                                     alt='Member image'
                                     fluid />
                                 </div>
@@ -44,7 +40,11 @@ const Members = () => {
                                     <MDBCardTitle>{member.name}</MDBCardTitle>
                                     <MDBCardText>{member.title}</MDBCardText>
                                     <div className="d-flex pt-1">
-                                    <MDBBtn outline className="me-1 flex-grow-1">Chat</MDBBtn>
+                                        <button type="button" className="btn btn-primary" style={{width: "150px"}}>
+                                            <a rel="noreferrer" style={{color : "white"}} target="_blank" href={member.whatsapp}>
+                                                Chat
+                                            </a>
+                                        </button>
                                     </div>
                                 </div>
                                 </div>
