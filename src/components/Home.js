2459c82fe6282771b9816c645627a5abd25582b1
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2023-05-18 23:37:15 +0530

    home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Page_1 from '../files/Page_1.jpg';
+import Page_2 from '../files/Page_2.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{marginTop: "50px"}}>
+            <img src={Page_1} className="App-logo" alt="logo" width="100%" height="100%"/>
+            <img src={Page_2} className="App-logo" alt="logo" width="100%" height="100%"/>
         </div>
     )
 }
